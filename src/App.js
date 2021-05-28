0dd00f6482dfd688df26d46189dab6ac57ecfded
S M Kaiser Ahmed <53963906+mekaiser@users.noreply.github.com>
2021-05-29 03:10:51 +0600

    components creation using objects

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import './App.css';
 
 function App() {
+  const nayoks = ['Anwar', 'Jafor', 'Alomgir', 'Salman'];
+  const products = [
+    {name: 'Photoshop', price: '$90.99'},
+    {name: 'Illustrator', price: '$60.99'},
+    {name: 'PDF Reader', price: '$6.99'}
+  ]
   return (
     <div className="App">
       <header className="App-header">
         <p>I am a React Person</p>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product>
+        <Person name="Munna" job="football"></Person>
+        <Person name="Masud" job="Dorshok"></Person>
         <Person></Person>
       </header>
     </div>
   );
 }
 
-function Person(){ // this is a component named Person
-  const personStyle = {
-    border: '2px solid red',
-    margin: '10px'
+function Product(props){
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '250px',
+    width: '250px',
+    float: 'left'
   }
+  const {name, price} = props.product;
+  return(
+    <div style={productStyle}>
+      <h3>{name}</h3>
+      <h5>{price}</h5>
+      <button>Buy now</button>
+    </div>
+  )
+}
+
+function Person(props){ // props is the property of Person component which is being passed as a parameter
   return (
-  <div style={{border: '2px solid yellow', margin: '10px', padding: '10px'}}>
-    <h1>Name: Shahrukh Khan</h1>
-    <h3>Hero of the year</h3>
-  </div>
+    <div style={{border: '2px solid gold', width: '400px'}}>
+      <h3>My Name: {props.name}</h3>
+      <p>My Profession: {props.job}</p>
+    </div>
   )
 }
 
