f74d29b2af6eea36b5ad7805ebb06578862ca631
S M Kaiser Ahmed <53963906+mekaiser@users.noreply.github.com>
2021-05-29 20:58:22 +0600

    state handler added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const nayoks = ['Anwar', 'Jafor', 'Alomgir', 'Salman'];
+  const nayoks = ['Razzak', 'Jasim', 'Alomgir', 'Salman', 'Bappi', 'Shuvo'];
   const products = [
     {name: 'Photoshop', price: '$90.99'},
     {name: 'Illustrator', price: '$60.99'},
-    {name: 'PDF Reader', price: '$6.99'}
+    {name: 'PDF Reader', price: '$6.99'},
+    {name: 'Premiere El', price: '$249.99'},
   ]
+  const nayokNames = nayoks.map(nayok => nayok);
   return (
     <div className="App">
       <header className="App-header">
         <p>I am a React Person</p>
+        <Counter></Counter>
+        <Users></Users>
+        <ul>
+          {
+            nayoks.map(nayok => <li>{nayok}</li>)
+          }
+          {
+            products.map(product => <li>{product.name}</li>)
+          }
+          {/* <li>{nayoks[0]}</li>
+          <li>{nayoks[1]}</li>
+          <li>{nayoks[2]}</li>
+          <li>{nayoks[3]}</li> */}
+        </ul>
+        {
+          products.map(pd => <Product product={pd}></Product>)
+        }
         <Product product={products[0]}></Product>
         <Product product={products[1]}></Product>
         <Product product={products[2]}></Product>
@@ -20,6 +40,40 @@ function App() {
       </header>
     </div>
   );
+}
+
+function Counter(){
+  const [count, setCount] = useState(10);
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  )
+}
+function Users(){
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
+
+  return(
+    <div>
+      <h3>Dynamic Users: {users.length}</h3>
+      <ul>
+        {
+          console.log(users)
+        }
+        {
+          users.map(user => <li>{user.email}</li>)
+        }
+      </ul>
+    </div>
+  )
 }
 
 function Product(props){
